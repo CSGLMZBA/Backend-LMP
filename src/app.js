7d006aa7ba2aacab407ef99ec6676ac40d1f530b
Ignacio Salas R <146854109+Nacho-SR@users.noreply.github.com>
2026-05-14 11:46:58 -0600

    [ADDED]: Servidor corriendo y conexion a firestore lista

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  return res.json({
+    success: true,
+    message: 'API funcionando',
+  });
+});
+
+export default app;
