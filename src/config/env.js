b21bd5aef60dd485f9bf7b1ed0c4f460a41cc675
Carlos Martinez <141792555+CSGLMZBA@users.noreply.github.com>
2026-05-14 22:14:59 -0600

    [FIXED]: the expiration time for the token is now specified on the env file with a default of 15m

--- src/config/env.js
+++ src/config/env.js
@@ -6,7 +6,7 @@ export const env = {
   PORT: process.env.PORT || 3000,
 
   JWT_SECRET: process.env.JWT_SECRET,
-
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '15m',
   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
   FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
   FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
