0991c054e8466b40fe52e7536ebad1ca11d2a7fb
Carlos Martinez <141792555+CSGLMZBA@users.noreply.github.com>
2026-05-14 22:41:51 -0600

    [Fixed]: salt rounds is now an integer

--- src/config/env.js
+++ src/config/env.js
@@ -7,7 +7,7 @@ export const env = {
 
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '15m', // Added so the program actually uses the specified time in the env file, if its not there just default to 15m
-  BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS || 10, // Added so salt rounds of hashing are no longer a magic number, with the default of 10 if not specified on the env file
+  BCRYPT_SALT_ROUNDS: parseInt(process.env.BCRYPT_SALT_ROUNDS || 10), // Added so salt rounds of hashing are no longer a magic number, with the default of 10 if not specified on the env file and turn it into an integer
   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
   FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
   FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
