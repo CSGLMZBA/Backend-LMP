b9cf5e1e41ee9b5a13686c84d65c3a1b906907f0
Carlos Martinez <141792555+CSGLMZBA@users.noreply.github.com>
2026-05-25 19:17:16 -0600

    [REMOVED]: import

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -3,7 +3,6 @@ import { Router } from 'express';
 import * as authController from './auth.controller.js';
 import { authMiddleware } from '../../middleware/auth.middleware.js';
 import { validate } from '../../middleware/validate.middleware.js';
-import { authorize } from '../../middleware/authorize.middleware.js';
 import { userSchema } from './auth.schema.js';
 
 
