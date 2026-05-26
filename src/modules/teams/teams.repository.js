6bf9fc7b75e5d9eb8d30daf815bd492ba6961624
Ignacio Salas R <146854109+Nacho-SR@users.noreply.github.com>
2026-05-26 13:13:59 -0600

    [ADDED]: Funciones para ayudar en la refactor para la logica de miembros de equipo por roles

--- src/modules/teams/teams.repository.js
+++ src/modules/teams/teams.repository.js
@@ -19,6 +19,48 @@ export const createTeamMember = async (data) => {
   };
 };
 
+export const findTeamMember = async (teamId, userId) => {
+  const snapshot = await db
+    .collection(teamMembersCollectionName)
+    .where('teamId', '==', teamId)
+    .where('userId', '==', userId)
+    .limit(1)
+    .get();
+
+  if (snapshot.empty) {
+    return null;
+  }
+
+  return {
+    id: snapshot.docs[0].id,
+    ...snapshot.docs[0].data(),
+  };
+};
+
+export const getTeamMembersByTeamId = async (teamId) => {
+  const snapshot = await db
+    .collection(teamMembersCollectionName)
+    .where('teamId', '==', teamId)
+    .get();
+
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
+
+export const getTeamMembersByUserId = async (userId) => {
+  const snapshot = await db
+    .collection(teamMembersCollectionName)
+    .where('userId', '==', userId)
+    .get();
+
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
+
 export const getTeamById = async (teamId) => {
   const teamDoc = await db.collection(teamsCollectionName).doc(teamId).get();
   if (!teamDoc.exists) {
